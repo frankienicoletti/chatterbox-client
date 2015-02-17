7cf53de025b63c73d1c3de18116d268ba9351db6
Kate Jefferson <barbedbella@gmail.com>
2015-02-16 18:31:06 -0600

    Work on displaying messages on the page.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -6,22 +6,27 @@ var message = {
   'roomname': '4chan'
 };
 
-// $.ajax({
-//   // always use this url
-//   url: 'https://api.parse.com/1/classes/chatterbox',
-//   type: 'POST',
-//   data: JSON.stringify(message),
-//   contentType: 'application/json',
-//   success: function (data) {
-//     console.log('chatterbox: Message sent');
-//   },
-//   error: function (data) {
-//     // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-//     console.error('chatterbox: Failed to send message');
-//   }
-// });
+function sendMessage(){
+  // HAS NOT BEEN COMPLETED YET
+  // $.ajax({
+  //   // always use this url
+  //   url: 'https://api.parse.com/1/classes/chatterbox',
+  //   type: 'POST',
+  //   data: JSON.stringify(message),
+  //   contentType: 'application/json',
+  //   success: function (data) {
+  //     console.log('chatterbox: Message sent');
+  //   },
+  //   error: function (data) {
+  //     // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+  //     console.error('chatterbox: Failed to send message');
+  //   }
+  // });
+}
 
-var messageRec = $.ajax({
+
+function getMessages(){
+  $.ajax({
   // always use this url
   url: 'https://api.parse.com/1/classes/chatterbox',
   type: 'GET',
@@ -34,19 +39,28 @@ var messageRec = $.ajax({
     // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
     console.error('chatterbox: Failed to get message');
   }
-});
-console.log(messageRec);
+  });
+}
 
 function readMessages(messageObj){
   var messages = messageObj['results'];
-  for (var i = 0; i < messages.length; i++){
-    console.log("User: ",messages[i]['username'],"   Message: ",messages[i]['text'],"   Room: ",messages[i]['room']);
+  for (var i = 0; i < 100; i++){
+    console.log(i, messages[i]['text']);
+    var msg = $().add("<span>").addClass("message").text(messages[i]['text']).add("<br>");
+    // "User: ",messages[i]['username'],"   Message: ",messages[i]['text'],"   Room: ",messages[i]['room']);
+    $(".messages").append(msg);
   }
+  //$("body").append("<div>HELLO</div>").addClass("messages");
 }
-
-
 
 
 var setMessage = function(message) {
   console.log(message.val());
 };
+
+$(document).ready(function() {
+  $("body").append("<div class='messages'></div>");
+  getMessages();
+});
+
+
